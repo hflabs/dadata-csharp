9be80882474fa643a618a305f416ce7df831e6c1
Anton Zhiyanov <m@antonz.ru>
2023-07-07 14:13:19 +0300

    impr: find address division parameter

--- Dadata/Model/FindAddressRequest.cs
+++ Dadata/Model/FindAddressRequest.cs
@@ -2,7 +2,9 @@
 {
     public class FindAddressRequest : SuggestRequest
     {
+        public AddressDivision division { get; set; }
         public string language { get; set; }
+
         public FindAddressRequest(string query, int count = 1) : base(query, count) { }
     }
 }
